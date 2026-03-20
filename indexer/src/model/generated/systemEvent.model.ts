eddce149c0d6ce0198547c6c8253a711cbed7f4b
Czpyioe <czpyioe@gmail.com>
2026-03-20 19:07:46 +0100

    fix(indexer): generate models with squid-typeorm-codegen
    Signed-off-by: Czpyioe <czpyioe@gmail.com>

--- indexer/src/model/generated/systemEvent.model.ts
+++ indexer/src/model/generated/systemEvent.model.ts
@@ -1,4 +1,4 @@
-import {Entity as Entity_, PrimaryColumn as PrimaryColumn_, StringColumn as StringColumn_, Index as Index_, IntColumn as IntColumn_, DateTimeColumn as DateTimeColumn_, BigIntColumn as BigIntColumn_} from "@subsquid/typeorm-store"
+import {Entity as Entity_, Column as Column_, PrimaryColumn as PrimaryColumn_, StringColumn as StringColumn_, Index as Index_, IntColumn as IntColumn_, DateTimeColumn as DateTimeColumn_, BigIntColumn as BigIntColumn_} from "@subsquid/typeorm-store"
 
 @Entity_()
 export class SystemEvent {
