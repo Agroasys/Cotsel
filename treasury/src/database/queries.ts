0db5919c0fa826d1d12c5d9dc87ecbf7a2469302
Aston Steven <astonsteven65@gmail.com>
2026-09-02 14:25:19 +0300

    fix(treasury): define query public surface
    Signed-off-by: Aston Steven <astonsteven65@gmail.com>

--- treasury/src/database/queries.ts
+++ treasury/src/database/queries.ts
@@ -2,12 +2,57 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-export * from './queries/accountingPeriods';
-export * from './queries/accountingProjections';
-export * from './queries/bankPayouts';
-export * from './queries/fiatDeposits';
-export * from './queries/ingestion';
-export * from './queries/ledger';
-export * from './queries/sweepBatches';
-export * from './queries/treasuryClaims';
-export * from './queries/treasuryPartnerHandoffs';
+export {
+  createAccountingPeriod,
+  getAccountingPeriodById,
+  listAccountingPeriods,
+  updateAccountingPeriodStatus,
+} from './queries/accountingPeriods';
+export {
+  getLedgerEntryAccountingFacts,
+  getLedgerEntryAccountingProjection,
+  listLedgerEntryAccountingProjections,
+} from './queries/accountingProjections';
+export { upsertBankPayoutConfirmation } from './queries/bankPayouts';
+export {
+  getFiatDepositByProviderEventId,
+  upsertFiatDepositReference,
+} from './queries/fiatDeposits';
+export {
+  consumeServiceAuthNonce,
+  getIngestionOffset,
+  setIngestionOffset,
+} from './queries/ingestion';
+export {
+  appendPayoutState,
+  getLatestBankPayoutConfirmation,
+  getLatestPayoutState,
+  getLedgerEntries,
+  getLedgerEntryById,
+  getLedgerEntryByTradeId,
+  listDistinctLedgerTradeIds,
+  upsertLedgerEntryWithInitialState,
+} from './queries/ledger';
+export {
+  addSweepBatchEntry,
+  createSweepBatch,
+  getPartnerHandoffByBatchId,
+  getSweepBatchById,
+  getSweepBatchDetail,
+  getTreasuryClaimEventByBatchId,
+  getTreasuryClaimEventByTxHash,
+  listSweepBatchEntries,
+  listSweepBatches,
+  updateSweepBatchStatus,
+} from './queries/sweepBatches';
+export {
+  createRevenueRealization,
+  upsertPartnerHandoff,
+  upsertTreasuryClaimEvent,
+} from './queries/treasuryClaims';
+export {
+  appendTreasuryPartnerHandoffEvidence,
+  getTreasuryPartnerHandoffByLedgerEntryId,
+  listTreasuryPartnerHandoffEventsByLedgerEntryId,
+  upsertTreasuryPartnerHandoff,
+} from './queries/treasuryPartnerHandoffs';
